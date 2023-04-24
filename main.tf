94e62baae6a257abda1fc7b8c53ea7e032cdf9a1
thlesaux <lesaux.thomas@gmail.com>
2023-04-24 23:19:46 +0200

    add main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+# Configure GCP project
+provider 'google' {
+  project = 'laravel-hello-world-384413'
+}
+# Deploy image to Cloud Run
+resource 'google_cloud_run_service' 'mywebapp' {
+  name     = 'mywebapp'
+  location = 'europe-west9'
+  template {
+    spec {
+      containers {
+        image = 'eu.gcr.io/laravel-hello-world-384413/webapp'
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+# Create public access
+data 'google_iam_policy' 'noauth' {
+  binding {
+    role    = 'roles/run.invoker'
+    members = [
+      'allUsers',
+    ]
+  }
+}
+# Enable public access on Cloud Run service
+resource 'google_cloud_run_service_iam_policy' 'noauth' {
+  location    = google_cloud_run_service.mywebapp.location
+  project     = google_cloud_run_service.mywebapp.project
+  service     = google_cloud_run_service.mywebapp.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+# Return service URL
+output 'url' {
+  value = '${google_cloud_run_service.mywebapp.status[0].url}'
+}
